99da514c0cb0f21b1ab1dd48c85ce4f43101f9bb
Razvan Chitu <razvan3895@yahoo.com>
2015-05-01 17:53:47 +0300

    IT WORKS!

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 build:
-	g++ Map.cpp test.cpp MapObject.cpp -o tetris
-run:
+	g++ -o tetris -Wall test.cpp Map.cpp MapObject.cpp `pkg-config --cflags --libs allegro-5.0 allegro_acodec-5.0 allegro_audio-5.0 allegro_color-5.0 allegro_dialog-5.0 allegro_font-5.0 allegro_image-5.0 allegro_main-5.0 allegro_memfile-5.0 allegro_physfs-5.0 allegro_primitives-5.0 allegro_ttf-5.0`
+run: build
 	./tetris
+
--- test.cpp
+++ test.cpp
@@ -1,8 +1,44 @@
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
 #include <stdio.h>
 #include <iostream>
 #include "Map.h"
 #include <string>
 using namespace std;
+
+#define TILE_SIZE   16
+
+ALLEGRO_DISPLAY *display = NULL;
+
+
+void numberToRGB(int code, int rgb[3]) {
+    switch(code) {
+        case RED:
+            rgb[0] = 100, rgb[1] = 0, rgb[2] = 0;
+            break;
+        case BLUE:
+            rgb[0] = 0, rgb[1] = 0, rgb[2] = 100;
+            break;
+        case CYAN:
+            rgb[0] = 0, rgb[1] = 255, rgb[2] = 255;
+            break;
+        case GREEN:
+            rgb[0] = 0, rgb[1] = 255, rgb[2] = 0;
+            break;
+        case ORANGE:
+            rgb[0] = 255, rgb[1] = 102, rgb[2] = 0;
+            break;
+        case PURPLE:
+            rgb[0] = 255, rgb[1] = 0, rgb[2] = 255;
+            break;
+        case YELLOW:
+            rgb[0] = 255, rgb[1] = 255, rgb[2] = 0;
+            break; 
+        case EMPTY:
+            rgb[0] = 0, rgb[1] = 0, rgb[2] = 0;
+            break;
+    }
+}
 
 class Game {
 private:
@@ -110,13 +146,46 @@ public:
             }
         std::cout << '\n';
     }
+
+    void draw() {
+        int rgb[3];
+        al_clear_to_color(al_map_rgb(0,0,0));
+        al_draw_filled_rectangle(20, 20, 620, 40, al_map_rgb(224, 223, 219));
+        al_draw_filled_rectangle(20, 40, 40, 40 + left.height * TILE_SIZE, al_map_rgb(224, 223, 219));
+        al_draw_filled_rectangle(600, 20, 620 , 40 + left.height * TILE_SIZE, al_map_rgb(224, 223, 219));
+        al_draw_filled_rectangle(20, 40 + left.height * TILE_SIZE, 620, 60 + left.height * TILE_SIZE, al_map_rgb(224, 223, 219));
+        al_draw_filled_rectangle(20, 40 + left.height * TILE_SIZE, 620, 60 + left.height * TILE_SIZE, al_map_rgb(224, 223, 219));
+        al_draw_filled_rectangle(40 + left.width * TILE_SIZE, 40 , 320 + (left.width - right.width) * TILE_SIZE / 2, 40 + left.height * TILE_SIZE, al_map_rgb(224, 223, 219));
+        for(int i = 0; i < left.height; ++i) {
+            for(int j = 0; j < left.width; ++j) {
+                numberToRGB(left.tiles[i][j], rgb);
+                al_draw_filled_rectangle(40 + j * TILE_SIZE, 40 + i * TILE_SIZE, 40 + (j+1) * TILE_SIZE,
+                    40 + (i+1) * TILE_SIZE, al_map_rgb(rgb[0], rgb[1], rgb[2]));
+
+            }
+            for(int j = 0; j < right.width; ++j) {                
+                numberToRGB(right.tiles[i][j], rgb);
+                al_draw_filled_rectangle(600 - (j+1) * TILE_SIZE, 40 + i * TILE_SIZE, 600 - j * TILE_SIZE,
+                    40 + (i+1) * TILE_SIZE, al_map_rgb(rgb[0], rgb[1], rgb[2]));
+            }
+
+        }
+        al_flip_display();
+    }
 };
 
 int main() {
+    al_init();
+    display = al_create_display(640, 480);
+
+    al_clear_to_color(al_map_rgb(0,0,0));
+
+    al_flip_display();
     string op;
-    Game g(7, 10);
+    Game g(10, 15);
     g.initGame();
     while(!g.gameOver()) {
+        g.draw();
         g.represent();
         cin >> op;
         if(op == "w")
@@ -135,9 +204,9 @@ int main() {
             g.rotateB();
         if(op == "h")
             g.moveForwardB();
-
-        // if(op == "chk")
-            // cout << map.columnIsFull(9);
+        if(op == "draw") {
+            g.draw();
+        }
     }
     return 0;
 }
